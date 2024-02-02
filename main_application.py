f4ffb8e500f3dd9bbaa26df0c6c4cd035e998583
DailyBreadWorkStation <ppsdevicestesting2@gmail.com>
2024-02-02 15:20:29 -0800

    create-series

---
+++ main_application.py
@@ -0,0 +1,17 @@
+from pages import DeviceListPage
+import time
+
+class MainApplication:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def open_device_list_page(self, url):
+        self.driver.get(url)
+        return DeviceListPage(self.driver,self)
+    
+    def normal_wait(self):
+        time.sleep(3)
+
+    def high_wait(self):
+        time.sleep(10)
+
